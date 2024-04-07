1591371b7bb68e9d11b2b93da9a75d3f2f1b3af5
Ajay Nischal <ajaynischal@vt.edu>
2024-04-06 19:06:21 -0700

    swift application added

---
+++ countdownapplication/countdownapplication/countdownapplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  countdownapplicationApp.swift
+//  countdownapplication
+//
+//  Created by Ajay Nischal(UST,US) on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct countdownapplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
